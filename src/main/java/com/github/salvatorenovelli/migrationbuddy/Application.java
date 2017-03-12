686ed347fa29d84856cf2ea6e97dc32a0bbd17c7
snovelli <salvatore.novelli@gmail.com>
2017-03-12 20:31:59 +0000

    Migrating to a spring boot application

--- src/main/java/com/github/salvatorenovelli/migrationbuddy/Application.java
+++ src/main/java/com/github/salvatorenovelli/migrationbuddy/Application.java
@@ -5,25 +5,31 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.analyticsreporting.v4.model.DateRange;
 import com.google.api.services.analyticsreporting.v4.model.GetReportsResponse;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 
 import java.io.File;
 
 import static java.util.Collections.singletonList;
 
-public class Application {
+public class Application implements CommandLineRunner {
 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
         JsonFactory jacksonFactory = JacksonFactory.getDefaultInstance();
-        Credential credentials = new GoogleAnalyticsCredentialFactory(new File(args[0]), jacksonFactory).authorize();
+        Credential credentials = new GoogleAnalyticsCredentialFactory(new File(strings[0]), jacksonFactory).authorize();
         AnalyticsService analyticsService = new AnalyticsService(credentials, jacksonFactory);
         analyticsService.init();
 
-
         String view_id = "93074237";
 
-
-        int numOfDays = 10;
-
+        int numOfDays = 30;
 
         DateRange completeRange = new DateRange();
         completeRange.setStartDate("2017-02-01");
@@ -31,15 +37,7 @@ public class Application {
 
         GetReportsResponse report = analyticsService.run(view_id, singletonList(completeRange));
         new ReportPrinter(report).print();
-
-        for (int i = 1; i <= 10; i++) {
-            DateRange dateRange = new DateRange();
-            dateRange.setStartDate("2017-02-" + String.format("%02d", i));
-            dateRange.setEndDate("2017-02-" + String.format("%02d", i));
-            GetReportsResponse report1 = analyticsService.run(view_id, singletonList(dateRange));
-            new ReportPrinter(report1).print();
-        }
-
-
     }
+
+
 }
